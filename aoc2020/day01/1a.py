b472942a29c9c75c792e64cf3e49b96cf3645d38
Scott Odle <scott@sjodle.com>
2020-11-30 22:32:10 -0700

    organize code into python modules with shared library

--- aoc2020/day01/1a.py
+++ aoc2020/day01/1a.py
@@ -1,8 +1,4 @@
-def read_input() -> [int]:
-    with open('input.txt') as input_file:
-        puzzle_input = input_file.read()
-        lines = puzzle_input.split('\n')
-        return [int(line) for line in lines if len(line) > 0]
+from ..shared import read_input
 
 
 def main():

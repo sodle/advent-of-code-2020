0a1cbc749d26491d2e77ab12abc8449a67cfa073
Scott Odle <scott@sjodle.com>
2020-11-30 22:19:46 -0700

    day 1

---
+++ 01/part1.py
@@ -0,0 +1,18 @@
+def read_input() -> [int]:
+    with open('input.txt') as input_file:
+        puzzle_input = input_file.read()
+        lines = puzzle_input.split('\n')
+        return [int(line) for line in lines if len(line) > 0]
+
+
+def main():
+    expenses = read_input()
+    for e1 in expenses:
+        for e2 in expenses:
+            if e1 + e2 == 2020:
+                print(f"{e1 * e2}")
+                return
+
+
+if __name__ == "__main__":
+    main()

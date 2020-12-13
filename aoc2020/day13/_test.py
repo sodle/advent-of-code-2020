0d9d858d54381e1ab029d5312a3937748f23a633
Scott Odle <scott@sjodle.com>
2020-12-13 13:22:11 -0700

    Day 13; part 1 works; part 2 works for test cases but not real input

---
+++ aoc2020/day13/_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from . import bezout, part1, part2
+
+
+class Test(unittest.TestCase):
+    def test_bezout(self):
+        assert bezout(1785, 546) == (-11, 36)
+        assert bezout(3, 20) == (7, -1)
+        assert bezout(4, 15) == (4, -1)
+        assert bezout(5, 12) == (5, -2)
+
+    def test_part1(self):
+        assert part1("7,13,x,x,59,x,31,19", 939) == 295
+
+    def test_part2(self):
+        assert part2("7,13,x,x,59,x,31,19") == 1068781
+        assert part2("17,x,13,19") == 3417
+        assert part2("67,7,59,61") == 754018
+        assert part2("67,x,7,59,61") == 779210
+        assert part2("67,7,x,59,61") == 1261476
+        assert part2("1789,37,47,1889") == 1202161486
+
+    if __name__ == '__main__':
+        unittest.main()
